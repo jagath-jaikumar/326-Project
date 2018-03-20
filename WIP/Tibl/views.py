e96d95123f59cd525f007eb7d350f699f25a91c6
jagath-jaikumar <jjaikumar@wpi.edu>
2018-03-20 10:57:04 -0400

    added URL mappings

--- WIP/Tibl/views.py
+++ WIP/Tibl/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Course, Section, Post, Student, Teacher, Department
+
+def index(request):
+    return render(
+        request,
+        'index.html',
+    )
+
+def classpage(request):
+    return render(
+        request,
+        'classpage.html',
+    )
+
+def messaging(request):
+    return render(
+        request,
+        'messaging.html',
+    )
+
+def myprofile(request):
+    return render(
+        request,
+        'myprofile.html',
+    )
+
+def friendprofile(request):
+    return render(
+        request,
+        'friendprofile.html',
+    )
