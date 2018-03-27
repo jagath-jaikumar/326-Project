8bab297c6bfb36330336cbe736bd144bf3a0ff51
Adam Rivelli <asrivelli@gmail.com>
2018-03-27 17:07:58 -0400

    incremental progress - get posts working

--- WIP/Tibl/views.py
+++ WIP/Tibl/views.py
@@ -11,9 +11,11 @@ def index(request, pk):
     section_list = Section.objects.filter(students=pk).filter(season='Sp').filter(year=2018)
     have_messaged = [message.receiver for message in Message.objects.filter(sender=pk)] +\
                     [message.sender for message in Message.objects.filter(receiver=pk)]
-
     # remove non-distinct elements
     have_messaged = list(set(have_messaged))
+
+    posts = Post.objects.filter(id__in=section_list)
+    print(posts[0].content)
 
     return render(
         request,
@@ -23,6 +25,7 @@ def index(request, pk):
         'section_list': section_list,
         'have_messaged': have_messaged,
         'cur_user': pk,
+        'posts': posts
         }
     )
 
