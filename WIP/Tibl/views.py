f508fa298e9c877c486d6e10c2b3186df6a6ea7e
Adam Rivelli <asrivelli@gmail.com>
2018-04-11 15:46:04 -0400

    update homepage to actually work, and to work with users

--- WIP/Tibl/views.py
+++ WIP/Tibl/views.py
@@ -1,19 +1,38 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Course, Section, Post, Student, Teacher, Department, Message 
+from django.contrib.auth.decorators import login_required
+import operator
+from .models import Course, Section, Post, Student, Teacher, Department, Message, User 
+import datetime
 
-def index(request, pk):
-    past_section_list = Section.objects.filter(students=pk).filter(~(Q(year=2018) & Q(season='Sp')))
-    section_list = Section.objects.filter(students=pk).filter(season='Sp').filter(year=2018)
-    have_messaged = [message.receiver for message in Message.objects.filter(sender=pk)] +\
-                    [message.sender for message in Message.objects.filter(receiver=pk)]
 
-    other_classmates = Student.objects.filter(section__in=section_list).filter(~Q(pk=pk)).distinct()
+@login_required
+def index(request):
+    poster = list(Student.objects.filter(user=request.user.pk))[0]
+    student_pk = poster.pk
+    if request.method == 'POST':
+        course_number = request.POST['class_name'].split(' ')[-1]
+        course_department = ' '.join(request.POST['class_name'].split(' ')[0:-1])
+        print(course_number)
+        print(course_department)
+        post_section = Section.objects.filter(course__department__name=course_department, course__number=course_number)
+        post_section = list(post_section)[0]
+        cur_post = Post(content=request.POST["post_text"], creation_date=datetime.datetime, 
+                        poster=poster, section=post_section)
+        cur_post.save()
+
+    past_section_list = Section.objects.filter(students=student_pk).filter(~(Q(year=2018) & Q(season='Sp')))
+    section_list = Section.objects.filter(students=student_pk).filter(season='Sp').filter(year=2018)
+    have_messaged = [message.receiver for message in Message.objects.filter(sender=student_pk)] +\
+                    [message.sender for message in Message.objects.filter(receiver=student_pk)]
+
+    other_classmates = Student.objects.filter(section__in=section_list).filter(~Q(pk=student_pk)).distinct()
 
     # remove non-distinct elements
     have_messaged = list(set(have_messaged))
 
-    posts = Post.objects.filter(id__in=section_list)
+    posts = Post.objects.filter(section__in=section_list)
+    ordered = posts.order_by('-creation_date')
 
     return render(
         request,
@@ -22,14 +41,11 @@ def index(request, pk):
         'past_section_list': past_section_list,
         'section_list': section_list,
         'have_messaged': have_messaged,
-        'cur_user': pk,
-        'posts': posts, 
+        'cur_user': student_pk,
+        'posts': ordered, 
         'other_classmates': other_classmates,
         }
     )
-
-def default_index(request):
-    return index(request, pk=1)
 
 def classpage(request, num_):
 
@@ -58,7 +74,6 @@ def messaging(request, profile_id):
     )
 
 def myprofile(request, profile_id):
-
     student = Student.objects.get(id = profile_id)
     sections = Section.objects.filter(students__id__exact=profile_id)
 
